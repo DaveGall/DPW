c4ca5a5feb7597d90c416ceab080307fcdf4b98b
David Gall <dlaker74@fullsail.edu>
2015-04-01 12:20:42 -0700

    Set up variables
    Set up and printed some variables in pycharm and the terminal.

--- Intro to Python/main.py
+++ Intro to Python/main.py
@@ -2,3 +2,9 @@
 '''
 Doc Strings, multiple lines of comments.
 '''
+#Don't need semicolons in python unless you are putting more than one thing on a single line.
+#Style guide: http://legacy.python.org/dev/peps/pep-0008
+first_name = "Dave"
+last_name = "Gall"
+#Python 3.0 uses parenthesis not a space like 2.7
+print first_name
