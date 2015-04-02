898e2598641dfdd198b0bceedc44439c332f36d3
David Gall <dlaker74@fullsail.edu>
2015-04-01 23:19:51 -0700

    While Loop
    Wrote my while loop to do a little count down from 5-0.

--- Madlib/madlib.py
+++ Madlib/madlib.py
@@ -9,10 +9,10 @@ slimy = raw_input("Name something slimy? ")
 adjective = raw_input("Name a great adjective? ")
 person_name1 = raw_input("Give me a persons name? ")# Names will be part of the array.
 person_name2 = raw_input("Give me another persons name? ")
-person_name3 = raw_input("Give me just one more persons name? ")'''
+person_name3 = raw_input("Give me just one more persons name? ")
 number1 = raw_input("Please give me a number between 1-10? ")
 number2 = raw_input("What is your favorite number? ")
-number3 = raw_input("How many people are there in your immediate family? ")
+number3 = raw_input("How many people are there in your immediate family? ")'''
 
 #print name+", "+favorite_meal+", "+kitchen_utensil+", "+verb+", "+favorite_animal+", "+slimy+", "+adjective+", "+person_name1+", "+person_name2+", "+person_name3+", "+number1+", "+number2+", "+number3+"."
 
@@ -25,7 +25,12 @@ def numberAdd(a,b):
     return add
 
 
-mult = numberMultiply(number1,number2)
-added = numberAdd(number3,number2)
+#mult = numberMultiply(number1,number2)
+#added = numberAdd(number3,number2)
 
-print number1+" * "+number2+" is: "+str(mult)+". "+number3+" + "+number2+" is: "+str(added)
+#print number1+" * "+number2+" is: "+str(mult)+". "+number3+" + "+number2+" is: "+str(added)
+
+i=5
+while i>=0:
+    print i
+    i = i-1
