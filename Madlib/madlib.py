43236c752b24d101738782bb9cc6011cdac5eae9
David Gall <dlaker74@fullsail.edu>
2015-04-01 23:31:10 -0700

    Added an array
    Added an array variable to catch the names that the user inputs and
    wrote a string that could possibly be used in my story.

--- Madlib/madlib.py
+++ Madlib/madlib.py
@@ -6,16 +6,18 @@ kitchen_utensil = raw_input("Name a kitchen utensil? ")
 verb = raw_input("Name the first verb you think of? ")
 favorite_animal = raw_input("What is your favorite animal? ")
 slimy = raw_input("Name something slimy? ")
-adjective = raw_input("Name a great adjective? ")
+adjective = raw_input("Name a great adjective? ")'''
 person_name1 = raw_input("Give me a persons name? ")# Names will be part of the array.
 person_name2 = raw_input("Give me another persons name? ")
 person_name3 = raw_input("Give me just one more persons name? ")
-number1 = raw_input("Please give me a number between 1-10? ")
-number2 = raw_input("What is your favorite number? ")
-number3 = raw_input("How many people are there in your immediate family? ")'''
+#number1 = raw_input("Please give me a number between 1-10? ")
+#number2 = raw_input("What is your favorite number? ")
+#number3 = raw_input("How many people are there in your immediate family? ")
 
+#Check to see that all data is collected.
 #print name+", "+favorite_meal+", "+kitchen_utensil+", "+verb+", "+favorite_animal+", "+slimy+", "+adjective+", "+person_name1+", "+person_name2+", "+person_name3+", "+number1+", "+number2+", "+number3+"."
 
+#number functions start
 def numberMultiply(a,b):
     times = int(a)*int(b)
     return times
@@ -23,14 +25,23 @@ def numberMultiply(a,b):
 def numberAdd(a,b):
     add = int(a)+int(b)
     return add
+#End number functions
 
+'''
+Number functions use is here.
+mult = numberMultiply(number1,number2)
+added = numberAdd(number3,number2)
 
-#mult = numberMultiply(number1,number2)
-#added = numberAdd(number3,number2)
-
-#print number1+" * "+number2+" is: "+str(mult)+". "+number3+" + "+number2+" is: "+str(added)
-
+print number1+" * "+number2+" is: "+str(mult)+". "+number3+" + "+number2+" is: "+str(added)
+'''
+#While loop begins
 i=5
 while i>=0:
     print i
     i = i-1
+#While loop ends.
+#Friends array begins here
+friends = [person_name1,person_name2,person_name3]
+print "Your friends "+friends[0]+", "+friends[1]+" and "+friends[2]+", will be joining us for dinner."
+
+#Friends array ends here
