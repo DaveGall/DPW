f3498d48a52036f36a01d3fa92fffaa7c5ee6cb6
David Gall <dlaker74@fullsail.edu>
2015-04-02 13:39:19 -0700

    Conditional statement
    Set up and checked my first conditional statement and it works
    correctly so far.

--- Madlib/madlib.py
+++ Madlib/madlib.py
@@ -5,17 +5,18 @@ favorite_meal = raw_input("What is your favorite meal? ")
 kitchen_utensil = raw_input("Name a kitchen utensil? ")
 verb = raw_input("Name the first verb you think of? ")
 favorite_animal = raw_input("What is your favorite animal? ")
-slimy = raw_input("Name something slimy? ")
 adjective = raw_input("Name a great adjective? ")
 person_name1 = raw_input("Give me a persons name? ")# Names will be part of the array.
 person_name2 = raw_input("Give me another persons name? ")
 person_name3 = raw_input("Give me just one more persons name? ")
-number1 = raw_input("Please give me a number between 1-10? ")
+number1 = raw_input("Please give me a number between 50-100? ")
 number2 = raw_input("What is your favorite number? ")
 number3 = raw_input("How many people are there in your immediate family? ")
+cond1 = raw_input("Do you like sports? y/n? ")
+cond2 = raw_input("Do you like apples? y/n?")
 
 #Check to see that all data is collected.
-#print name+", "+favorite_meal+", "+kitchen_utensil+", "+verb+", "+favorite_animal+", "+slimy+", "+adjective+", "+person_name1+", "+person_name2+", "+person_name3+", "+number1+", "+number2+", "+number3+"."
+#print name+", "+favorite_meal+", "+kitchen_utensil+", "+verb+", "+favorite_animal+", "+", "+adjective+", "+person_name1+", "+person_name2+", "+person_name3+", "+number1+", "+number2+", "+number3+"."
 
 #number functions start
 def numberMultiply(a,b):
@@ -28,7 +29,7 @@ def numberAdd(a,b):
 #End number functions
 #begin number function variables
 
-Number functions use is here.
+#Number functions use is here.
 mult = numberMultiply(number1,number2)
 added = numberAdd(number3,number2)
 
@@ -46,3 +47,27 @@ friends = [person_name1,person_name2,person_name3]
 print "Your friends "+friends[0]+", "+friends[1]+" and "+friends[2]+", will be joining us for dinner."
 
 #Friends array ends here
+#Conditional statements Begin
+if cond1 == "y" or cond1 == "Y":
+    sports = "You will also be traveling with your favorite sports team!!"
+    print sports
+else:
+    band = "You will be traveling with your favorite musical group!!"
+    print band
+
+
+
+#Conditional statements End
+
+
+
+
+
+
+
+
+
+
+
+
+
