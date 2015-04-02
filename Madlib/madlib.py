9e999667539128cc276d580663c2693d5350c92f
David Gall <dlaker74@fullsail.edu>
2015-04-01 18:31:26 -0700

    Creating Data Collectors
    Setting up some raw input data collectors to get some information from
    the user.

---
+++ Madlib/madlib.py
@@ -0,0 +1,12 @@
+#David Gall, 04/01/15, My MadLib story.
+#Collect some information here.
+name = raw_input("What is your name? ")
+favorite_meal = raw_input("What is your favorite meal? ")
+kitchen_utensil = raw_input("Name a kitchen utensil? ")
+verb = raw_input("Name the first verb you think of? ")
+favorite_animal = raw_input("What is your favorite animal? ")
+slimy = raw_input("Name something slimy? ")
+adjective = raw_input("Name a great adjective? ")
+person_name1 = raw_input("Give me a persons name? ")# Names will be part of the array.
+person_name2 = raw_input("Give me another persons name? ")
+person_name3 = raw_input("Give me just one more persons name? ")
