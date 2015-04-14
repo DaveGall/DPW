faa33bae96c507738d64ea661642c9762cff6a9f
David Gall <dlaker74@fullsail.edu>
2015-04-13 17:37:08 -0700

    Follow along video
    How to display the information that you get from user input in
    different places, console, page.

--- login-practice/main.py
+++ login-practice/main.py
@@ -9,21 +9,32 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        page = '''<!DOCTYPE HTML>
+        page_head = '''<!DOCTYPE HTML>
 <html>
     <head>
         <title>Simple Form</title>
     </head>
-    <body>
-        <form method="GET">
+    <body>'''
+
+
+        page_body = '''<form method="GET">
             <label>Name:</label> <input type="text" name="user"/>
             <label>Email:</label> <input type="text" name="email"/>
-            <input type="submit" value="Submit"/>
-        </form>
+            <input type="submit" value="Submit"/>'''
+
+        page_close = '''</form>
     </body>
 </html>
         '''
-        self.response.write(page)
+        if self.request.GET:
+            #storing the information from user.
+            user = self.request.GET['user']
+            email = self.request.GET['email']
+            self.response.write(page_head+ user+' '+email+page_body+page_close)
+        else:
+            self.response.write(page_head+page_body+page_close)
+        #print "Hello there."prints to console
+        #self.response.write(page) prints the information out to the page.
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
