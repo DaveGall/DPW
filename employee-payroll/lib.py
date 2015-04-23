5078930fd0ef5f4d1622242ad061e0654101f722
David Gall <dlaker74@fullsail.edu>
2015-04-23 13:49:19 -0700

    Reusable Library
    Setting up the Payroll class which will house the calculations for the
    user input of hours, deductions and pay.

--- employee-payroll/lib.py
+++ employee-payroll/lib.py
@@ -1,3 +1,13 @@
 __author__ = 'davegall'
 
+'''
+Payroll class for my employee hours tracker.
+'''
 
+class Payroll(object):
+    def __init__(self):
+        self.employee_name = ""
+        self.employee_hours = 0
+        self.employee_dependants = 0
+        self.hourly_pay = 0
+        self.__employee_check = 0
--- employee-payroll/page.py
+++ employee-payroll/page.py
@@ -6,8 +6,8 @@ Page class for my employee hours tracker.
 
 class Page(object):
     def __init__(self):
-        self.employee_name = ""
-        self.employee_hours = 0
-        self.employee_dependants = 0
+        self.head = ""
+        self.body = ""
+        self.close =""
 
 
