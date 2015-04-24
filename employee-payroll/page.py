6ca002a22ffce0936667eb2793e62f7f55c40473
David Gall <dlaker74@fullsail.edu>
2015-04-23 21:40:18 -0700

    Reusable Library
    Creating variables for each of the pieces of information I want to put
    back into the results body so that I can structure them and place them
    in the correct place.

--- employee-payroll/page.py
+++ employee-payroll/page.py
@@ -55,8 +55,8 @@ class Page(object):
         self.close = '''
             <footer>
                 <ul>
-                    <li></li>
-                    <li></li>
+                    <li>Link 1</li>
+                    <li>Link 2</li>
                 </ul>
             </footer>
         </div>
@@ -73,18 +73,34 @@ class FinalBody(object):
         self.new_body = '''
             <div class="container">
             '''
-    def results_name(self):'''
+        new_body = self.new_body
+        return new_body
+    def results_name(self):
+        self.new_name = '''
                 <p>User name will go here.</p>
                 '''
-    def results_hours(self):'''
+        new_name = self.new_name
+        return new_name
+    def results_hours(self):
+        self.new_hours = '''
                 <p>Users hours will go here.</p>
                 '''
+        new_hours = self.new_hours
+        return new_hours
+    def results_users(self):
+        self.rate = '''
                 <p>Users rate of pay will go here.</p>
+                '''
+    def results_gross(self):
+        self.new_gross = '''
                 <p>Users gross paycheck.</p>
+                '''
+    def results_net(self):
+        self.new_net = '''
                 <p>Users net paycheck.</p>
             </div>
         '''
-        new_body = self.new_body
-        return new_body
+        #new_body = self.new_body+self.new_name+self.new_hours+self.rate+self.new_gross+self.new_net
+        #return new_body
 
 
