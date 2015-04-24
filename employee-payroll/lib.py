7472f1d0eaed7b6ada642d25950a32d506ccc62c
David Gall <dlaker74@fullsail.edu>
2015-04-24 16:33:01 -0700

    Reusable Library
    Commenting out my code. Half way through my Library file.

--- employee-payroll/lib.py
+++ employee-payroll/lib.py
@@ -3,24 +3,24 @@
 
 #Payroll class for my employee hours tracker.
 
-class EmployeeCheck(object):
+class EmployeeCheck(object):#This begins my functions class that will handle all calculations and can be used for any instance.
     def __init__(self):
         pass
 
-    def deductions(self, d):
-        if int(d) < 4:
+    def deductions(self, d):# this decides which tax rate to use.
+        if int(d) < 4:#if you have less than 4 dependants then your total tax rate will be .23 which equates to 23%
             rate = .23
-            return rate
-        else:
+            return rate#This returns the decimal to be multiplied by the employees gross pay.
+        else:           #if your dependants are over 4 you will be charged at .28 which equates to 28%
             rate = .28
-            return rate
+            return rate#This returns the decimal to be multiplied by the gross of the employees pay.
 
-    def check_gross(self, hours, pay):
-        self.gross = float(hours) * float(pay)
-        gross = self.gross
-        return gross
+    def check_gross(self, hours, pay):# This function will give us the result of the users gross pay.
+        self.gross = float(hours) * float(pay)#This multiples the two arguments and puts them into a variable called self.gross
+        gross = self.gross#This takes self.gross and puts it into an easier to return variable.
+        return gross#This returns the results of the equation.
 
-    def employee_taxes(self, g, r):
+    def employee_taxes(self, g, r):#This function gives us how much the employee paid in total taxes.
         self.taxes = float(g) * float(r)
         taxes = self.taxes
         return taxes
