c1d5e3ef6b0d6cbb56e0ba059af65f6877f751fc
David Gall <dlaker74@fullsail.edu>
2015-04-29 22:11:36 -0700

    Dynamic Website
    Created an instance of my Laker class with kareem abdul jabbar stats

--- laker-greats/data.py
+++ laker-greats/data.py
@@ -10,4 +10,15 @@ class Lakers(object):
         self.year_drafted = 0
 
 
+class Data(object):
+    def __init__(self):
+        kareem = Lakers()
+        kareem.number = 33
+        kareem.name = 'Kareem Abdul-Jabbar'
+        kareem.game_average = 24.6
+        kareem.career_points = 38,387
+        kareem.position = 'Center'
+        kareem.year_drafted = 1976
+
+        
 
